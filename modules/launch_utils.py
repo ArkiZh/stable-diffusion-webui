8ade0bd1589d381fb327284d30e1d27ea1b35ef0
ArkiZh <1797466468@qq.com>
2023-08-19 16:20:44 +0800

    DEBUG.

--- modules/launch_utils.py
+++ modules/launch_utils.py
@@ -155,7 +155,7 @@ def git_clone(url, dir, name, commithash=None):
         run(f'"{git}" -C "{dir}" fetch', f"Fetching updates for {name}...", f"Couldn't fetch {name}")
         run(f'"{git}" -C "{dir}" checkout {commithash}', f"Checking out commit for {name} with hash: {commithash}...", f"Couldn't checkout commit {commithash} for {name}", live=True)
         return
-
+    print(f"===== COMMAND: git clone {url} {dir}")
     run(f'"{git}" clone "{url}" "{dir}"', f"Cloning {name} into {dir}...", f"Couldn't clone {name}", live=True)
 
     if commithash is not None:
--- webui-user.bat
+++ webui-user.bat
@@ -4,5 +4,5 @@ set PYTHON=
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
-
+set ACCELERATE=True
 call webui.bat
